a41c98bc2b19fde90d0051fd207a978176a6d530
Madhvi N <madhvinandanwar@gmail.com>
2025-02-15 22:21:36 +0530

    Frontend changes

--- static/frontend/reddit-app/src/environments/environment.prod.ts
+++ static/frontend/reddit-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   baseUrl: '/api/v1/',
-  serverUrl: 'http://localhost:8000',
-  appUrl: 'http://localhost:4200',
-  loginUrl: 'http://localhost:4200/sign-in',
+  serverUrl: 'https://django-reddit.onrender.com',
+  appUrl: 'https://madhvi-n.github.io/django_reddit',
+  loginUrl: 'https://madhvi-n.github.io/django_reddit/sign-in',
   staticUrl: '../assets/images/'
 };
